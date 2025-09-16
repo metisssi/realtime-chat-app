4bd3b1fb6238fc50294ce9aa857ccb66bdbee1a4
metisssi <simcheraadnrij@gmail.com>
2025-09-16 20:08:26 +0200

    message routes optimazed

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -49,10 +49,23 @@ export const getMessagesByUserId = async (req, res) => {
 export const sendMessage = async (req, res) => {
     try {
         const { text, image } = req.body;
-
         const { id: receiverId } = req.params
-
         const senderId = req.user._id
+
+        if(!text && !image) {
+            return res.status(400).josn({ message: "Text or image is required"})
+        }
+
+        if (senderId.equals(receiverId)) {
+            return res.status(404).json({ message: "Recevier not found"})
+        }
+
+        const receiverExists = await User.exists({ _id: receiverId})
+        if(!receiverExists) {
+            return res.status(400).json({ message: 'Recevier not found.'})
+        }
+
+
 
 
         let imageUrl
